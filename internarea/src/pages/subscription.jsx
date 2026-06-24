f8a85f6b177e84a8c962b1117156543f1932abc1
sakshipoojary14 <sakshipoojary12@gmail.com>
2026-06-24 23:33:11 +0530

    trigger vercel redeploy

--- internarea/src/pages/subscription.jsx
+++ internarea/src/pages/subscription.jsx
@@ -1,9 +1,7 @@
-import subscription from "../components/subscription";
-
+import SubscriptionPage from "../components/subscription.jsx";
 export default function Subscription() {
   return <SubscriptionPage />;
 }
-
   return (
     <div>
       <h1>subscription</h1>
