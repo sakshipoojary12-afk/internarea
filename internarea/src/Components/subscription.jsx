49521558c5fe5df07f4dc6fc90e9d390575d624f
sakshipoojary14 <sakshipoojary12@gmail.com>
2026-06-24 23:35:28 +0530

    trigger vercel redeploy

---
+++ internarea/src/Components/subscription.jsx
@@ -0,0 +1,12 @@
+import SubscriptionPage from "./subscription";
+
+export default function Subscription() {
+  return <SubscriptionPage />;
+}
+  return (
+    <div>
+      <h1>subscription</h1>
+    </div>
+  );
+
+  return <subscription />;
