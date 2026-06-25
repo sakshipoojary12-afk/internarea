d0a9cf7723d4b19bd884aa27d7ddd1be555f18a0
sakshipoojary14 <sakshipoojary12@gmail.com>
2026-06-25 10:54:36 +0530

    remove brevo and restore nodemailer

--- backend/Routes/resume.js
+++ backend/Routes/resume.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
 let otpStore = {};
 
 // SEND OTP
